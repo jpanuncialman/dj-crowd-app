06043cbdd1c38f278602e0fb9da2c0742f1704ec
jpanuncialman <joe@panuncialman.com>
2022-03-18 00:05:06 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    fetch('https://api.twitch.tv/helix/users?login=bubucore69', {
+      method: "GET",
+      headers: {
+        "Client-ID": "e4d8gb8osi3kbahha86mf1hajtzy8e",
+        "Authorization": "Bearer w25plakq3zpltlyd2dyb1b0zrko53z"
+      }
+    })
+    .then(response => {
+        console.log("RESPONSE: ", response)
+    })
+    .catch(error => {
+        // handle the error
+        console.log(error)
+    });
+
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
