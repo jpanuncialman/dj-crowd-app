39390ecfb22b14fdeabd2e6bd3ef9467fd76ec3a
jpanuncialman <joe@panuncialman.com>
2022-03-18 21:00:02 -0400

    Changed image

--- src/Body.js
+++ src/Body.js
@@ -3,7 +3,7 @@ import body0 from './assets/body0.gif';
 import body1 from './assets/body1.gif';
 import body2 from './assets/body2.gif';
 import body3 from './assets/body3.png';
-import body4 from './assets/body4.gif';
+import body4 from './assets/body4.png';
 import body5 from './assets/body5.png';
 
 const imgMap = [body0, body1, body2, body3, body4, body5];
